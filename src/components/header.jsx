007e642b491073c75f1bfa82efeaf4fbd9bfad5d
DESKTOP-NT9H6LD\hirai <hirai@j-cool.fm>
2023-05-16 10:54:31 +0900

    ヘッダーコンポーネント設計

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+// import topMv01 from "./assets/image/topMv01.jpg";
+// import "./assets/scss/Top.scss";
+
+function Header() {
+  return (
+    <>
+      <header className="header">a</header>
+    </>
+  );
+}
+
+export default Header;
