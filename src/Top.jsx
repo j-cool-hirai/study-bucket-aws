5881ee0afed770dbbcca050202ee8e8c1b0ad72e
DESKTOP-NT9H6LD\hirai <hirai@j-cool.fm>
2023-05-16 10:22:29 +0900

    swiper設定

--- src/Top.jsx
+++ src/Top.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import topMv01 from "./assets/image/topMv01.jpg";
 import "./assets/scss/Top.scss";
 import "swiper/swiper-bundle.css";
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
-SwiperCore.use([Navigation, Pagination, Autoplay]);
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Autoplay,
+  EffectFade,
+} from "swiper";
+SwiperCore.use([Navigation, Pagination, Autoplay, EffectFade]);
 
 function App() {
   return (
@@ -14,24 +19,28 @@ function App() {
           <Swiper
             autoplay={{ delay: 3000, disableOnInteraction: false }}
             loop={true}
-            effect={"fade"}
-            speed={1000}
-            fadeEffect={{
-              crossFade: true,
-            }}
+            effect="fade" // effectの値を文字列で指定
+            speed={2000}
             containerClass="swiper-container"
-            onSwiper={(swiper) => {
-              swiper.update();
-            }}
+            fadeEffect={{ crossFade: true }}
           >
             <SwiperSlide>
               <img src={topMv01} alt="ソースカツ丼1" />
             </SwiperSlide>
             <SwiperSlide>
-              <img src={topMv01} alt="ソースカツ丼2" />
+              <img
+                src={"https://cdn.snapdish.co/photo/596c546cda21f213d1cfbb19"}
+                alt="ソースカツ丼2"
+              />
             </SwiperSlide>
             <SwiperSlide>
               <img src={topMv01} alt="ソースカツ丼3" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src={"https://cdn.snapdish.co/photo/596c546cda21f213d1cfbb19"}
+                alt="ソースカツ丼4"
+              />
             </SwiperSlide>
           </Swiper>
         </div>
